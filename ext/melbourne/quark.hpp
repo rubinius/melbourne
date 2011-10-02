20d17cc7d10a7c72b0ed9d142f50f606453a7cf5
Bryan Helmkamp <bryan@brynary.com>
2011-10-01 22:35:06 -0400

    Import Melbourne from rubinius/rubinius

---
+++ ext/melbourne/quark.hpp
@@ -0,0 +1,15 @@
+#ifndef MEL_QUARK_HPP
+#define MEL_QUARK_HPP
+
+#include <stddef.h>
+
+namespace melbourne {
+  typedef size_t quark;
+  enum {QUARK_NOT_FOUND = ~0L};
+
+  quark quark_from_string(const char* str);
+  const char* quark_to_string(const quark quark);
+  void quark_cleanup();
+}
+#endif
+
